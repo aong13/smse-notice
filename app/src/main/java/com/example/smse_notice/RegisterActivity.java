9830af7421984872fadebc914939826e3f25b7e1
aong13 <aong13@gmail.com>
2023-08-22 02:51:35 +0900

    Feat : 로그인, 회원가입(1) UI
    - 앱바 뒤로가기
    - 학생증 인증 미구현
    - 회원가입 2로 넘어가는 기능 구현 O
    - 로그인 -> 회원가입 이동 구현 O

--- app/src/main/java/com/example/smse_notice/RegisterActivity.java
+++ app/src/main/java/com/example/smse_notice/RegisterActivity.java
@@ -1,14 +1,45 @@
 package com.example.smse_notice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView toolbar_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //뒤로가기 버튼
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //toolbar title 변경
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //기존 title 제거
+        toolbar_title = (TextView) findViewById(R.id.toolbar_title);
+        toolbar_title.setText("회원가입");
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                //액티비티 이동
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
