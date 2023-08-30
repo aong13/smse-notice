8abaca4fa8f3ce0852d9d32dbfab9f3ec54e74e2
aong13 <aong13@gmail.com>
2023-08-30 17:23:48 +0900

    Feat : 생년월일 선택 달력 버튼 추가, 학년 스피너 기능 추가
    - gradle, manifest 파일 수정 (Retrofit)
    - xml id, activity 변수 추가
    - 생년월일 달력
    - 학년 스피너(integer array)

--- app/src/main/java/com/example/smse_notice/RegisterActivity2.java
+++ app/src/main/java/com/example/smse_notice/RegisterActivity2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -16,24 +16,30 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.Calendar;
-
 public class RegisterActivity2 extends AppCompatActivity {
 
     TextView toolbar_title, dateText;
     EditText id_signup, pwd_signup, checkPwd_signup, studentNum_signup, email_signup, year_signup, month_signup, date_signup;
-    Spinner grade_signup;
+    Spinner grade_spinner = null;
     Button doneBtn_signup;
     ImageView calenderBtn;
-    DatePickerDialog datePickerDialog;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+
+    ArrayAdapter<Integer> adapter = null;
+    Integer[] grade_array = new Integer[]{1,2,3,4};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
 
 
+        year_signup = findViewById(R.id.year_signup);
+        month_signup = findViewById(R.id.month_signup);
+        date_signup = findViewById(R.id.date_signup);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         //뒤로가기 버튼
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -43,15 +49,35 @@ public class RegisterActivity2 extends AppCompatActivity {
         toolbar_title = findViewById(R.id.toolbar_title);
         toolbar_title.setText("회원가입");
 
-
-
-
+        //스피너
+        adapter = new ArrayAdapter(RegisterActivity2.this, android.R.layout.simple_spinner_dropdown_item, grade_array);
+        grade_spinner = findViewById(R.id.grade_signup);
+        grade_spinner.setAdapter(adapter);
 
         //회원가입 완료 버튼
-        Button doneBtn_signup= findViewById(R.id.doneBtn_signup);
+        doneBtn_signup= findViewById(R.id.doneBtn_signup);
         doneBtn_signup.setOnClickListener(view -> {
-            Intent signupDoneIntent = new Intent(RegisterActivity2.this, LoginActivity.class);
+            Intent signupDoneIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            signupDoneIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(signupDoneIntent);
+        });
+
+        //날짜 버튼 클릭
+        calenderBtn=findViewById(R.id.calenderBtn_signup);
+        calenderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callbackMethod = new DatePickerDialog.OnDateSetListener(){
+                    public void onDateSet(DatePicker view, int y, int m, int d){
+                        year_signup.setText(String.valueOf(y));
+                        month_signup.setText(String.valueOf(m+1));
+                        date_signup.setText(String.valueOf(d));
+                    }
+                };
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterActivity2.this, callbackMethod, 2023, 8, 24);
+                datePickerDialog.show();
+            }
+
         });
 
     }
@@ -68,4 +94,5 @@ public class RegisterActivity2 extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
